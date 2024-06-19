89d76a692939b6890473b99843e8755625d947b2
jsendz <joshuasendzul@gmail.com>
2024-06-19 16:48:09 +0200

    larandom

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -24,7 +24,7 @@ export default function Form () {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Email</label>
-      <input type="email" name="email" id="email" className="p-3 rounded-md border border-gray-300 w-64 text-lg focus:outline-none focus:ring-2 focus:ring-blue-600" />
+      <input type="email" name="user_email" className="p-3 rounded-md border border-gray-300 w-64 text-lg focus:outline-none focus:ring-2 focus:ring-blue-600" />
       <input type="submit" value="Send" className=" px-6 py-3 bg-blue-600 text-white rounded-md text-lg hover:bg-blue-700"/>
       
     </form>
