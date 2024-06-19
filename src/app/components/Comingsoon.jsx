ffc45cfae2b91db17ed1a0d13c4d612442638ec7
jsendz <joshuasendzul@gmail.com>
2024-06-19 19:24:02 +0200

    larandom

--- src/app/components/Comingsoon.jsx
+++ src/app/components/Comingsoon.jsx
@@ -10,7 +10,7 @@ export default function ComingSoon() {
 
   return (
     <div className="w-full h-full">
-      <Image src="/ordena.jpeg" alt="founder's photo" width={1280} height={900} className="hidden lg:block" />
+      <Image src="/ordena.jpeg" alt="founder's photo" width={1280} height={900} className="hidden lg:block rounded-lg m-auto" />
       <Image src="/movil.jpeg" alt="founder's photo" width={390} height={900} className=" lg:hidden " />
 
      
